5aee7b41ebce78bb957ee263e2166c85f1c387f4
super <marcgalaxy02@gmail.com>
2023-03-27 16:21:02 +0200

    initial commit

---
+++ android/src/com/marc/dropgame/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.marc.dropgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.marc.dropgame.Drop;
+
+public class DesktopLauncher {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		initialize(new Drop(), config);
+	}
+}
